c416be1dc94c9c245c31afa8e47641d6bcf3302b
unknown <gisellerociogil@outlook.es>
2021-10-27 21:17:04 -0300

    CRUD

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -4,6 +4,15 @@ const fs = require('fs');
 /* Leer jsons y parsearlos */
 let jsonProducts = fs.readFileSync(path.resolve(__dirname, '../db/productos.json'), 'utf-8');
 let products = JSON.parse(jsonProducts); //json a array
+const nuevoId =()=>{
+    let ultimo=0;
+    products.forEach(element =>{
+        if (element.id > ultimo){
+            ultimo=element.id;
+        }
+    })
+        return ultimo +1;
+}
 
 let controller ={
     detalle : (req, res) =>{
@@ -22,8 +31,42 @@ let controller ={
     agregar: (req, res) =>{
         res.render("products/agregarProducto");
     },
+    agregarProducto : (req, res) => {
+        let productoNuevo ={
+            id: nuevoId(),
+            ... req.body,
+           };
+            products.push(productoNuevo)
+        let jsonProductos= JSON.stringify(products, null, 4);
+        fs.writeFileSync(path.resolve(__dirname, "../db/products.json"), jsonProductos);
+        res.redirect('/products/crud');
+    },
+
     editar: (req, res)=>{
-        res.render("products/editarProducto")
+        let productoAeditar=products.find(function (producto){
+            return producto.id==req.params.id
+        })
+        res.render("products/editarProducto", {productoAeditar});
+    },
+    update: (req, res) =>{
+        products.forEach(producto =>{
+            if (producto.id == req.params.id){
+              /* producto.name = req.body.name;
+                producto.description =req.body.description;
+                producto.price= req.body.price
+                producto.image= req.body.image
+                producto.category=req.body.category
+                producto.status= req.body.status 
+                console.log(req.body.name); */
+            }
+        
+    
+        })
+        let jsonProducts = JSON.stringify(products, null, 4);
+        fs.writeFileSync(path.resolve(__dirname, '../db/products.json'), jsonProducts);
+
+        res.redirect('/products/crud');
+
     },
     borrar: (req, res) => {
         let nuevaListaProductos = products.filter(item => {
--- src/routes/products.js
+++ src/routes/products.js
@@ -7,7 +7,11 @@ router.get("/detalle/:id", controller.detalle);
 router.get("/carrito", controller.carrito);
 router.get("/crud", controller.crud);
 router.get("/agregarProducto", controller.agregar);
+router.post("/agregarProducto", controller.agregarProducto);
 router.get("/editarProducto/:id", controller.editar);
+router.put("/editarProducto/:id", controller.update);
+
+
 router.delete("/editarProducto/:id", controller.borrar);
 
 module.exports=router;
