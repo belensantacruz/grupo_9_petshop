7b4ce20a75fef6dda6773aaa5121fe11e0965a7f
belensantacruz <belensantacruz95@gmail.com>
2022-01-19 20:38:44 -0300

    Fix para adminMiddleware

--- src/middlewares/adminMiddleware.js
+++ src/middlewares/adminMiddleware.js
@@ -1,8 +1,6 @@
 function adminMiddleware(req, res, next) {
     if (req.session.loggedUser && !req.session.loggedUser.isadmin)
         res.redirect('/users/profile') ;
-    else
-        res.redirect('/');
     next();
 }
 module.exports = adminMiddleware;
