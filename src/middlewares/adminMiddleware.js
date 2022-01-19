93164e87037652e4695a8cd2e091ba10cc95aff4
belensantacruz <belensantacruz95@gmail.com>
2022-01-19 20:41:45 -0300

    Modificacion adminMiddleware

--- src/middlewares/adminMiddleware.js
+++ src/middlewares/adminMiddleware.js
@@ -1,6 +1,8 @@
 function adminMiddleware(req, res, next) {
     if (req.session.loggedUser && !req.session.loggedUser.isadmin)
         res.redirect('/users/profile') ;
+    if (!req.session.loggedUser)
+        res.redirect('/');
     next();
 }
 module.exports = adminMiddleware;
