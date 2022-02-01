77d3e1d989658c903c81ddd1374830b27774755a
agushmda <ahermida97@gmail.com>
2022-01-31 23:51:27 -0300

    cambio de port para trabajar con react en paralelo

--- src/app.js
+++ src/app.js
@@ -27,4 +27,4 @@ app.use("/", mainRouter);
 app.use("/users", usersRouter);
 app.use("/products", productRouter);
 app.use("/api", apiRouter);
-app.listen(3000, () => console.log("Server corriendo en el puerto 3000"));
+app.listen(8080, () => console.log("Server corriendo en el puerto 8080"));
